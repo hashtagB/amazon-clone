4574c8c4b1b8e7a3fcd8ae0677d07c4197bb5c4c
bhargavimarudhuri <bhargavimarudhuri@gmail.com>
2021-05-26 18:38:50 -0400

    working on header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+        </div>
+    )
+}
+
+export default Header
+
