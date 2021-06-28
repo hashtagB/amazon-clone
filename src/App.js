622a219db5194c1e819f2ef19dccda3a2ff252a8
bhargavimarudhuri <bhargavimarudhuri@gmail.com>
2021-06-28 10:03:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,62 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import './App.css';
-import Header from './Header';
-import Home from './Home';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Checkout from "./Checkout";
+import Home from "./components/home/Home"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Header from "./components/header/Header"
+import Checkout from './components/checkout/Checkout';
+import Login from './components/login/Login';
+import {useStateValue} from "./components/stateProvider/StateProvider"
+import {auth} from "./firebase"
 
 function App() {
+
+  const [{user}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if(authUser) {
+        //The user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        })
+      } else {
+        //The user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+    })
+
+    return () => {
+      // Any clean up operation goes in here
+      unsubscribe();
+    }
+  }, [])
+
+  console.log("USER IS >>>>>", user)
   return (
     <Router>
-      <div className="app">
-      <Header />
-
+      <div className="App">
         <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
           <Route path="/checkout">
-            <Checkout /> 
+            <Header/>
+            <Checkout/>
           </Route>
-          
           <Route path="/">
-            <Home /> 
+            <Header/>
+            <Home/>
           </Route>
-
-        </Switch> 
-        
+        </Switch>
       </div>
     </Router>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAc0r-9TjDug3WocqfLGzHmA0YX3c7iJLY",
     authDomain: "clone-f8df8.firebaseapp.com",
@@ -8,3 +9,8 @@ const firebaseConfig = {
     appId: "1:683957654032:web:e92256b765d7b0fd1a3095",
     measurementId: "G-WM8GK069BS"
   };
+
+// const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
